c2d5362e2453fbe2c7dfa0e8cd333ddd0e16ba09
Nikita Ignatov <alamacus@gmail.com>
2019-06-05 18:51:47 +0300

    Minor fixes

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 
 from daemon import daemon
@@ -17,27 +18,31 @@ def ticket_handler(bot, job):
 
     ticket_list = get_tickets_from_emails(settings.IMAP, settings.LOGIN, settings.PASS, settings.FOLDER, settings.DEL_FOLDER)
 
-    if len(ticket_list) > 0:
-        
-        for ticket in ticket_list:
-            nmb = ticket['ticket']
-            theme = ticket['theme']
-            theme = theme.replace('[', '\[')
-            url = ticket['url']
-            action = ticket['action']
-            if action == 'Task created':
-                sign = emojize(':white_check_mark:', use_aliases=True)
-                reply = f'*Новая задача*{sign}\n{nmb}\n{theme}\n{url}'
+    now = datetime.now()
 
-            elif action == 'SLA expiring':
-                sign = emojize(':warning:', use_aliases=True)
-                reply = f'*Истекает SLA*{sign}\n{nmb}\n{theme}\n{url}'
+    if '10' < datetime.strftime(now, '%H') < '20':
 
-            elif action == 'SLA expired':
-                sign = emojize(':bangbang:', use_aliases=True)
-                reply = f'*Истёк SLA*{sign}\n{nmb}\n{theme}\n{url}'
-        
-            bot.send_message(chat_id='-1001361799205', text=reply, parse_mode=telegram.ParseMode.MARKDOWN)
+        if len(ticket_list) > 0:
+            
+            for ticket in ticket_list:
+                nmb = ticket['ticket']
+                theme = ticket['theme']
+                #theme = theme.replace('[', '\[')
+                url = ticket['url']
+                action = ticket['action']
+                if action == 'Task created':
+                    sign = emojize(':white_check_mark:', use_aliases=True)
+                    reply = f'*Новая задача*{sign}\n{nmb}\n*{theme}*\n{url}'
+
+                elif action == 'SLA expiring':
+                    sign = emojize(':warning:', use_aliases=True)
+                    reply = f'*Истекает SLA*{sign}\n{nmb}\n*{theme}*\n{url}'
+
+                elif action == 'SLA expired':
+                    sign = emojize(':bangbang:', use_aliases=True)
+                    reply = f'*Истёк SLA*{sign}\n{nmb}\n*{theme}*\n{url}'
+            
+                bot.send_message(chat_id='-1001361799205', text=reply, parse_mode=telegram.ParseMode.MARKDOWN)
 
 def set_support_duty(bot, update):
     pass
@@ -46,7 +51,7 @@ if __name__ == '__main__':
 
     bot = Updater(token=settings.TELEGRAM_API_KEY)
     jobs = bot.job_queue
-    job_minute = jobs.run_repeating(ticket_handler, interval=10, first=0)
+    job_minute = jobs.run_repeating(ticket_handler, interval=60, first=0)
     dp = bot.dispatcher
     bot.start_polling()
     bot.idle()
